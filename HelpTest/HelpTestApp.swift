23caad4202da78feb1ce70e0c537c1603bb2bd45
Freya Ariel <hifreyaariel@icloud.com>
2022-01-20 15:54:51 +0100

    Initial commit

---
+++ HelpTest/HelpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpTestApp.swift
+//  HelpTest
+//
+//  Created by Freya Ariel on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HelpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
